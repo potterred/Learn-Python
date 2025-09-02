dd28f88b3f1b4257008d51df30335b87be542b96
Morgana <morganaschnneider@gmail.com>
2025-09-01 22:20:25 -0300

    Add files via upload

---
+++ curso_python/aula.py
@@ -0,0 +1,14 @@
+"""
+Dosctring
+E escrever o que eu 
+quiser
+
+sadasdas
+"""
+''' Usar para escrever suas notas '''
+
+
+# Permite escrever um comentario
+print(123) # na frente
+# Abaixo
+print(123)
