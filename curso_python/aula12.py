951aab951f3d56c5349f85a6791161207c3c846a
unknown <morganaschnneider@gmail.comgit config --global user.name potterred>
2025-09-05 10:52:14 -0300

    adicionando arquivos

---
+++ curso_python/aula12.py
@@ -0,0 +1,19 @@
+nome = 'Morgana'
+altura = 1.63
+peso =  63
+imc =  peso / altura ** 2
+
+
+print(f'Morgana tem {altura} de altura')
+print(f'{peso} quilos e seu imc é: {imc:.2f} ')
+
+
+
+# print(nome, 'tem', altura, 'de altura',)
+# print('pesa',peso, 'quilos e seu imc é',)
+# print(imc)
+
+# f-strings
+# linha_1 = f'{nome} tem {altura:.2f} de altura,'
+# linha_2 = f'pesa {peso} quilos e seu imc é'
+# linha_3 = f'{imc:2f}'
