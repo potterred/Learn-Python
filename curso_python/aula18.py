b4014b5462249b00a0d795586dc9147312fb27d6
Potterred <morganaschnneider@gmail.com>
2025-09-12 21:03:20 -0300

    adicionando arquivos

---
+++ curso_python/aula18.py
@@ -0,0 +1,11 @@
+# Passo 1: Pegar o primeiro valor
+primeiro_valor = float(input("Digite um valor: "))
+
+# Passo 2: Pegar o segundo valor
+segundo_valor = float(input("Digite outro valor: "))
+
+# Passo 3: Comparar os dois valores
+if primeiro_valor >= segundo_valor:
+    print("O primeiro valor é maior ou igual ao segundo.")
+else:
+    print("O segundo valor é maior.")
