1aaf916feca5abaebfa540fed0a3cf595687adcf
Potterred <morganaschnneider@gmail.com>
2025-09-24 04:29:58 -0300

    py

--- curso_python/aula30.py
+++ curso_python/aula30.py
@@ -14,3 +14,10 @@ local_carro = 100  # local em que o carro está na estrada
 RADAR_1 = 60  # velocidade máxima do radar 1
 LOCAL_1 = 100  # local onde o radar 1 está
 RADAR_RANGE = 1  # A distância onde o radar pega
+
+if velocidade > RADAR_1:
+    print('Velocidade carro passou do radar')
+
+if local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+    local_carro <= (LOCAL_1 + RADAR_RANGE):
+    print('Carro multado em radar 1')
